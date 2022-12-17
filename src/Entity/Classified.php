bb5d866b09bcf273bd32946dd114555801b9c42d
Ugurkan Kaya <ugurkankya@gmail.com>
2022-12-17 01:59:19 +0100

    feat: add basic classified entity

---
+++ src/Entity/Classified.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\ClassifiedRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClassifiedRepository::class)]
+class Classified
+{
+    use EntityIdTrait;
+    use EntityCreatedAndUpdatedAtTrait;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $offerNumber = null;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getOfferNumber(): ?string
+    {
+        return $this->offerNumber;
+    }
+
+    public function setOfferNumber(?string $offerNumber): self
+    {
+        $this->offerNumber = $offerNumber;
+
+        return $this;
+    }
+}
