22f2254dd352695d647b7cc697d579bdd01a01bf
Ugurkan Kaya <ugurkankya@gmail.com>
2022-12-17 15:31:08 +0100

    feat: add show in search list and detail page for property group option

---
+++ migrations/Version20221217142732.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221217142732 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property_group_option ADD show_in_search_list TINYINT(1) NOT NULL, ADD show_in_detail_page TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE property_group_option DROP show_in_search_list, DROP show_in_detail_page');
+    }
+}
--- src/Entity/PropertyGroupOption.php
+++ src/Entity/PropertyGroupOption.php
@@ -32,6 +32,12 @@ class PropertyGroupOption
     #[ORM\OneToMany(mappedBy: 'groupOption', targetEntity: PropertyGroupOptionValue::class)]
     private Collection $optionValues;
 
+    #[ORM\Column]
+    private bool $showInSearchList = false;
+
+    #[ORM\Column]
+    private bool $showInDetailPage = false;
+
     public function __construct()
     {
         $this->optionValues = new ArrayCollection();
@@ -69,6 +75,42 @@ class PropertyGroupOption
     public function setOptionValues(Collection $optionValues): self
     {
         $this->optionValues = $optionValues;
+
+        return $this;
+    }
+
+    public function getPropertyGroup(): ?PropertyGroup
+    {
+        return $this->propertyGroup;
+    }
+
+    public function setPropertyGroup(?PropertyGroup $propertyGroup): self
+    {
+        $this->propertyGroup = $propertyGroup;
+
+        return $this;
+    }
+
+    public function isShowInSearchList(): bool
+    {
+        return $this->showInSearchList;
+    }
+
+    public function setShowInSearchList(bool $showInSearchList): self
+    {
+        $this->showInSearchList = $showInSearchList;
+
+        return $this;
+    }
+
+    public function isShowInDetailPage(): bool
+    {
+        return $this->showInDetailPage;
+    }
+
+    public function setShowInDetailPage(bool $showInDetailPage): self
+    {
+        $this->showInDetailPage = $showInDetailPage;
 
         return $this;
     }
