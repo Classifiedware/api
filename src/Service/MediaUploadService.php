71a566c3826eb3ab2e6c40d74e185bc7acf78c22
Ugurkan Kaya <27974830+ugurkankya@users.noreply.github.com>
2024-08-23 16:54:53 +0200

    feat: add creating of media thumbnails

--- src/Service/MediaUploadService.php
+++ src/Service/MediaUploadService.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Entity\Media;
+use App\Entity\MediaThumbnail;
 use Doctrine\ORM\EntityManagerInterface;
+use Intervention\Image\ImageManager;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
@@ -17,12 +19,15 @@ class MediaUploadService
 {
     public function __construct(
       private readonly Filesystem $filesystem,
+      private readonly ImageManager $imageManager,
       private readonly LoggerInterface $logger,
       private readonly EntityManagerInterface $entityManager,
       private readonly string $mediaDirRelative,
+      private readonly string $thumbnailDirRelative,
       private readonly string $mediaDir,
       private readonly string $thumbnailDir,
-      private readonly array $uploadAllowedExtensions
+      private readonly array $uploadAllowedExtensions,
+      private readonly array $thumbnailSizes
     ) {
         $this->createMediaDir();
         $this->createThumbnailDir();
@@ -65,24 +70,28 @@ class MediaUploadService
 
     private function saveMedia(UploadedFile $imageFile): ?Media
     {
-        $fileHash = $this->generateFileHash($imageFile);
-        $hashedPath = $this->generateHashedPath($fileHash);
-        $uploadPath = $this->mediaDir. '/'. $hashedPath;
-        $fileName = $this->generateHashedFileName($fileHash, $imageFile);
-        $mediaPath = sprintf('%s/%s/%s', $this->mediaDirRelative, $hashedPath, $fileName);
+        $fileHash = $this->generateFileHash($imageFile->getFilename());
+        $mediaPath = $this->generateHashedPath($this->mediaDir, $fileHash, $this->getFileExtension($imageFile));
+        $mediaPathRelative = str_replace($this->mediaDir, $this->mediaDirRelative, $mediaPath);
+        $fileName = pathinfo($mediaPath, PATHINFO_BASENAME);
+        $mediaDir = dirname($mediaPath);
 
-        if (!$this->filesystem->exists($uploadPath)) {
+        if (!$this->filesystem->exists($mediaDir)) {
             try {
-                $this->filesystem->mkdir($uploadPath);
+                $this->filesystem->mkdir($mediaDir);
             } catch (IOExceptionInterface $exception) {
                 $this->logger->error('Unable to create hashed media folder', ['path' => $exception->getPath()]);
             }
         }
 
         try {
-            $imageFile->move($uploadPath, $fileName);
+            $imageFile->move($mediaDir, $fileName);
 
-            return $this->createMediaEntity($mediaPath);
+            $media = $this->createMediaEntity($mediaPathRelative);
+
+            $this->createThumbnails($media, $mediaPath, $imageFile);
+
+            return $media;
         } catch (FileException) {
             $this->logger->error('Unable to move media file to its final path', ['path' => $mediaPath]);
         }
@@ -90,31 +99,78 @@ class MediaUploadService
         return null;
     }
 
-    private function generateHashedPath(string $fileHash): string
+    private function createThumbnails(Media $media, string $mediaPath, UploadedFile $imageFile): void
+    {
+        foreach ($this->thumbnailSizes as $thumbnailSize) {
+            [$width, $height] = explode('x', $thumbnailSize);
+
+            $this->createThumbnail($width, $height, $mediaPath, $media, $imageFile);
+        }
+    }
+
+    private function createThumbnail(string $width, string $height, string $mediaPath, Media $media, UploadedFile $imageFile): void
+    {
+        $thumbnailExtension = $this->getFileExtension($imageFile);
+        $fileHash = $this->generateFileHash(
+            sprintf(
+                '%s_%sx%s.%s',
+                $imageFile->getFilename(),
+                $width,
+                $height,
+                $thumbnailExtension
+            )
+        );
+        $thumbnailPath = $this->generateHashedPath($this->thumbnailDir, $fileHash, $thumbnailExtension);
+        $thumbnailPathRelative = str_replace($this->thumbnailDir, $this->thumbnailDirRelative, $thumbnailPath);
+        $thumbnailDir = dirname($thumbnailPath);
+
+        if (!$this->filesystem->exists($thumbnailDir)) {
+            try {
+                $this->filesystem->mkdir($thumbnailDir);
+            } catch (IOExceptionInterface $exception) {
+                $this->logger->error('Unable to create hashed thumbnail folder', ['path' => $exception->getPath()]);
+            }
+        }
+
+        $image = $this->imageManager->read($mediaPath);
+        $image->resize((int)$width, (int)$height);
+        $image->save($thumbnailPath);
+
+        $mediaThumbnail = new MediaThumbnail();
+        $mediaThumbnail->setUuid(Uuid::v4());
+        $mediaThumbnail->setPath($thumbnailPathRelative);
+        $mediaThumbnail->setMedia($media);
+        $mediaThumbnail->setWidth($width);
+        $mediaThumbnail->setHeight($height);
+        $mediaThumbnail->setCreatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($mediaThumbnail);
+        $this->entityManager->flush();
+    }
+
+    private function generateHashedPath(string $uploadPath, string $fileHash, string $extension): string
     {
         $firstHash = mb_substr($fileHash, 0, 2);
         $secondHash = mb_substr($fileHash, 2, 2);
         $thirdHash = mb_substr($fileHash, 4, 2);
 
         return sprintf(
-            '%s/%s/%s',
+            '%s/%s/%s/%s/%s.%s',
+            $uploadPath,
             $firstHash,
             $secondHash,
-            $thirdHash
+            $thirdHash,
+            $fileHash,
+            $extension
         );
     }
 
-    private function generateHashedFileName(string $fileHash, UploadedFile $file): string
-    {
-        return sprintf('%s.%s', $fileHash, $this->getFileExtension($file));
-    }
-
-    private function generateFileHash(UploadedFile $file): string
+    private function generateFileHash(string $fileName): string
     {
         return sha1(sprintf(
             '%s%s%s',
             bin2hex(random_bytes(6)),
-            $file->getFilename(),
+            $fileName,
             time()
         ));
     }
